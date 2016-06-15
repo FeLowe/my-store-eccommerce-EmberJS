aabfa5cb544f1dd9ab933b6c32f4c1542a5cbe9a
Farnoosh Johnson and Fernanda Lowe <farnoosh+fernanda@>
2016-06-15 16:17:03 -0700

    save entered items in the database and show a success message on item being added

--- app/components/admin-tile.js
+++ app/components/admin-tile.js
@@ -1,22 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  lastItemToSHow: Ember.computed('items.[]', function(){
+    return this.get('name') + ' with brand ' + this.get('brand');
+  }),
+
+  addItemFormSubmit: false,
   actions: {
     selectCategory(selection, component){
       if (selection) {
         this.set('category',selection);
-        debugger;
+
       }
     },
     addItem() {
       var params= {
-        category: this.get('category'),
-        brand:" temporary",
-        name: this.get('name'),
-        price: this.get('price'),
-        description: this.get('description'),
-        imageItem: this.get('imageItem')
+          category: this.get('category'),
+          brand:this.get('brand'),
+          name: this.get('name'),
+          price: this.get('price'),
+          description: this.get('description'),
+          imageItem: this.get('imageItem')
       };
+      this.set("addItemFormSubmit", true);
       this.sendAction("addItem", params);
     }
   }
